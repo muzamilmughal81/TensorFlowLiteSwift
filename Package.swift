e9cb014f1aa52f5fb8def14360b2190841739239
muzamil8101 <92907344+muzamil8101@users.noreply.github.com>
2025-03-11 04:20:00 +0500

    Added linker flags for -lc++

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TensorFlowLiteSwift",
-//            targets: ["TensorFlowLiteSwift", "TensorFlow"]),
-            targets: ["TensorFlow"]),
+            targets: ["TensorFlowLiteSwift", "TensorFlow"]),
+//            targets: ["TensorFlow"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
