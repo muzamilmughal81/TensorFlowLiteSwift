1abe3eec3a4e805b8664c598f9e2427c30360671
muzamil8101 <92907344+muzamil8101@users.noreply.github.com>
2025-03-11 03:51:42 +0500

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TensorFlowLiteSwift",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "TensorFlowLiteSwift",
+//            targets: ["TensorFlowLiteSwift", "TensorFlow"]),
+            targets: ["TensorFlow"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+//        .target(
+//            name: "TensorFlowLiteSwift"),
+        
+        .binaryTarget(
+                    name: "TensorFlow",
+                    path: "./Sources/TensorFlowLiteC.xcframework"
+                )
+
+    ]
+)
